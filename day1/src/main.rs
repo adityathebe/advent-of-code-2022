640596c7284cbddfaac17e29c8d64782230f7e7a
Aditya <thebeaditya@gmail.com>
2022-12-10 13:11:00 +0545

    Day 2 Complete

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,10 +1,40 @@
 fn main() {
     let contents = include_str!("../input.txt");
-    let lines = contents.lines();
 
     let mut current: i32 = 0;
+    let mut total_calories: Vec<i32> = Vec::new();
+
+    // Simply calculate the total of each elves
+    for line in contents.lines() {
+        if line == "" {
+            total_calories.push(current);
+            current = 0;
+            continue;
+        }
+
+        current += line.parse::<i32>().unwrap();
+    }
+
+    total_calories.push(current);
+    total_calories.sort();
+    total_calories.reverse();
+
+    let sum_of_three: i32 = total_calories.as_slice()[..3].iter().sum();
+    println!("Sum of top 3: {}", sum_of_three);
+}
+
+fn _part_a() {
+    let contents = include_str!("../input.txt");
+
+    // current is the total calories counted so far
+    // by a given Elf.
+    let mut current: i32 = 0;
+
+    // max is the max number of calories counted by
+    // one of Elves visted so far.
     let mut max: i32 = -1;
-    for line in lines {
+
+    for line in contents.lines() {
         if line == "" {
             if current > max {
                 max = current
@@ -14,11 +44,7 @@ fn main() {
             continue;
         }
 
-        let res = line.parse::<i32>();
-        match res {
-            Ok(x) => current += x,
-            Err(e) => println!("{e}"),
-        }
+        current += line.parse::<i32>().unwrap();
     }
 
     println!("max {max}")
